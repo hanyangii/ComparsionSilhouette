1809b8fa1fc31030a41b590162384dba08eee34c
yuni <id9595@naver.com>
2016-01-27 17:42:34 +0800

    complete basic skeleton : make shapes on the screen

--- DrawShape.py
+++ DrawShape.py
@@ -1,1 +1,29 @@
 #Drawing shapes function. It operate with label
+# 1=circle, 2=triangle, 3=square, 4=rectangle
+import matplotlib.pyplot as plt
+import math
+
+def DrawShape(image):
+	print image
+	if image.label ==1:
+		shape = plt.Circle((image.x, image.y), radius = 3, fc='y')
+	elif image.label == 2:
+		x=image.x
+		y=image.y
+		points=[[x-2*math.sqrt(3),y-math.sqrt(3)],[x,y+3],[x+2*math.sqrt(3),y-math.sqrt(3)]]
+		shape = plt.Polygon(points, angle=image.theta)
+	elif image.label == 3:
+		shape = plt.Rectangle((image.x, image.y),3*math.sqrt(2),3*math.sqrt(2),angle=image.theta,fc='r')
+	elif image.label == 4:
+		shape = plt.Rectangle((image.x, image.y),9*math.sqrt(2),3*math.sqrt(2),angle=image.theta,fc='green')
+	
+	return shape
+	
+
+def DrawReference(images, imageNum):
+	plt.axes()
+	for i in range(imageNum):
+		shape = DrawShape(images[i])
+		plt.gca().add_patch(shape)
+	plt.axis([0,30,0,30])
+	plt.show()
--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 #This is main skeleton of ComparisonShillhouette program
+from DrawShape import *
+import matplotlib.pyplot as plt
+
+class Image:
+	def __init__(self, label, x, y, theta):
+		self.label=label
+		self.x=x
+		self.y=y
+		self.theta=theta
+	
+	def __repr__(self):
+		return '<{} {} {} {}>'.format(self.label,self.x,self.y,self.theta)
+
+#read input file
+inputFile = open("reference.txt",'r')
+
+images = []
+referenceNum = int(inputFile.readline())
+
+for i in range(referenceNum):
+	line = inputFile.readline()
+	if not line: break
+	line = line.split(' ')
+	image = Image(int(line[0]),float(line[1]),float(line[2]),float(line[3]))
+	images.append(image)
+
+DrawReference(images, referenceNum)
